156032a7a502c566490e2e994d4a0875ea0ab687
AleeMunizz <aleemunizz@gmail.com>
2023-05-12 15:44:24 -0300

    Seção 2: Entity

--- Back/src/Proev.API/Controllers/EventoController.cs
+++ Back/src/Proev.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Proev.API.Data;
 using Proev.API.Models;
 
 namespace Proev.API.Controllers
@@ -12,40 +13,24 @@ namespace Proev.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public IEnumerable<Evento> _evento = new Evento[] {
-            new Evento() {
-                EventoID = 1,
-                Tema = "Angular 11 e .NET 5",
-                Local = "Campos do Jordão",
-                Lote = "1º Lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImageURL = "Foto.png"
-            },
-            new Evento() {
-                EventoID = 2,
-                Tema = "Angular 11 e Suas Novidades",
-                Local = "Campos do Jordão",
-                Lote = "2º Lote",
-                QtdPessoas = 200,
-                DataEvento = DateTime.Now.AddDays(5).ToString("dd/MM/yyyy"),
-                ImageURL = "Foto2.png"
-            }
-        };  
-        public EventoController()
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {  
+            _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {  
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("id")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {  
-            return _evento.Where(evento => evento.EventoID == id);
+            return _context.Eventos.FirstOrDefault(
+                evento => evento.EventoID == id
+            );
         }
 
         [HttpPost]
---
+++ Back/src/Proev.API/Data/Migrations/20230512173903_init.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Proev.API.data.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Eventos",
+                columns: table => new
+                {
+                    EventoID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Local = table.Column<string>(type: "TEXT", nullable: true),
+                    DataEvento = table.Column<string>(type: "TEXT", nullable: true),
+                    Tema = table.Column<string>(type: "TEXT", nullable: true),
+                    QtdPessoas = table.Column<int>(type: "INTEGER", nullable: false),
+                    Lote = table.Column<string>(type: "TEXT", nullable: true),
+                    ImageURL = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Eventos", x => x.EventoID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Eventos");
+        }
+    }
+}
